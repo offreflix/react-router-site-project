3425778fd282f66aec598873cb1615510d459fad
João Victor <reflixplayshd@gmail.com>
2022-03-14 15:33:35 -0300

    Install react-router and make new routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
 import Header from './components/Header';
-import Newsletter from './components/Newsletter';
+import Home from './components/Newsletter';
+import Services from './components/Services';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Newsletter />
+      <Home />
+      {/* <Services />
+      <About />
+      <Contact /> */}
     </div>
   );
 }
