85bf5ccb5907aa5ce1a3a8991a28c6caf13de4c7
João Victor <reflixplayshd@gmail.com>
2022-03-15 14:24:50 -0300

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,38 @@
+import './style/Contact.css';
+
+import { FiPhoneCall, FiMail, FiHome } from 'react-icons/fi';
+
 function Contact() {
+  const size = '4rem';
   return (
     <div className="contact">
       <div className="input">
         <input type="text" placeholder="Nome" />
         <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Insira sua Mensagem!" />
+        <textarea placeholder="Insira sua Mensagem!" />
         <button onClick={() => {}}>Enviar</button>
+      </div>
+      <div className="contactInfoContainer">
+        <div className="contactInfo">
+          <FiMail size={size} />
+          <p>
+            <span>Email</span>: empresa@gmail.com
+          </p>
+        </div>
+        <div className="contactInfo">
+          <FiPhoneCall size={size} />
+          <p>
+            <span>Telefone</span>: 0800 999 9999
+          </p>
+        </div>
+        <div className="contactInfo">
+          <FiHome size={size} />
+          <p>
+            Algum Lugar <br />
+            Alguma Rua <br />
+            Alguma Coisa
+          </p>
+        </div>
       </div>
     </div>
   );
