e3abbadef59273241d9c83c1fdcedcb99f95b9e7
João Victor <reflixplayshd@gmail.com>
2022-03-21 13:56:18 -0300

    added responsive navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,54 +1,86 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 import { NavLink } from 'react-router-dom';
 import './style/Header.css';
 
+import { FiAlignJustify, FiX } from 'react-icons/fi';
+
 function Header() {
+  const [sidebar, setSidebar] = useState(false);
+
+  function showSidebar() {
+    setSidebar(!sidebar);
+  }
+
   return (
     <header className="header">
       <a href="https://github.com/Reflixzinhu">
         <p>Logo Empresa</p>
       </a>
-
-      <ul className="nav">
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'selected' : '')}
-            to="/"
-          >
-            Início
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'selected' : '')}
-            to="/services"
-          >
-            Serviços
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'selected' : '')}
-            to="/about"
-          >
-            Sobre
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'selected' : '')}
-            to="/contact"
-          >
-            Contato
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup">
-            <span className="signup">Já tem uma conta?</span>
-          </NavLink>
-        </li>
-      </ul>
+      <nav>
+        {!sidebar ? (
+          <FiAlignJustify
+            size={'2rem'}
+            className="navIcon"
+            onClick={showSidebar}
+          />
+        ) : (
+          <FiX size={'2rem'} className="navIcon" onClick={showSidebar} />
+        )}
+        <div className={!sidebar ? 'navContainer' : 'navContainer clicked'}>
+          <div className="navBackground">
+            <ul className="nav">
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'selected' : '')}
+                  to="/"
+                  onClick={showSidebar}
+                >
+                  Início
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'selected' : '')}
+                  to="/services"
+                  onClick={showSidebar}
+                >
+                  Serviços
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'selected' : '')}
+                  to="/about"
+                  onClick={showSidebar}
+                >
+                  Sobre
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'selected' : '')}
+                  to="/contact"
+                  onClick={showSidebar}
+                >
+                  Contato
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'selected' : '')}
+                  to="/signup"
+                  onClick={showSidebar}
+                >
+                  <span className={'signup fullText'}>Já tem uma conta?</span>
+                  <span className={'signup shortText'}>Login</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
